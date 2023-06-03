9501476ce6b99a030a9d0296923f82bfc5c99544
JPiolho <783467+jpiolho@users.noreply.github.com>
2023-06-03 18:17:52 +0200

    Fixed routing issue by removing forward-slash at the beginning

--- JapaneseNumbersTrainer/Base/Routes.cs
+++ JapaneseNumbersTrainer/Base/Routes.cs
@@ -5,14 +5,14 @@ public static class Routes
     public const string MainMenu = "/";
     
     public const string NormalNumbersMenu = "/normal-numbers";
-    public const string NormalNumbersNumberToJapanese = $"/{NormalNumbersMenu}/number-to-japanese";
-    public const string NormalNumbersOptions = $"/{NormalNumbersMenu}/options";
-    public const string NormalNumbersTranslate = $"/{NormalNumbersMenu}/translate";
+    public const string NormalNumbersNumberToJapanese = $"{NormalNumbersMenu}/number-to-japanese";
+    public const string NormalNumbersOptions = $"{NormalNumbersMenu}/options";
+    public const string NormalNumbersTranslate = $"{NormalNumbersMenu}/translate";
 
     public const string DatesMenu = "/dates";
-    public const string DatesWeekdays = $"/{DatesMenu}/weekdays";
-    public const string DatesMonths = $"/{DatesMenu}/months";
-    public const string DatesDays = $"/{DatesMenu}/days";
-    public const string DatesFullDate = $"/{DatesMenu}/full-date";
+    public const string DatesWeekdays = $"{DatesMenu}/weekdays";
+    public const string DatesMonths = $"{DatesMenu}/months";
+    public const string DatesDays = $"{DatesMenu}/days";
+    public const string DatesFullDate = $"{DatesMenu}/full-date";
 
 }
